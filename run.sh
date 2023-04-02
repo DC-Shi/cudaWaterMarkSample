9cf030c3a51cde97c421e2d872165780d590cd2c
DC-Shi <daochen.shi@gmail.com>
2023-04-02 12:37:17 +0800

    Remove unused file

--- run.sh
+++ run.sh
@@ -6,12 +6,4 @@ export SMS=62
 
 make clean build
 
-make run ARGS="-input=data/Lena.pgm"
-
-
-if [ $? -eq 0 ]; then
-    echo OK, converting pgm files into png for easier view.
-    python3 ConvertImageToPng.py --folder ./data
-else
-    echo Run failed.
-fi
+make run ARGS="-input=data/color.png"
