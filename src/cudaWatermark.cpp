5d9ffe38f4a32456aba064fe5cfe98aa6d49aa71
DC-Shi <daochen.shi@gmail.com>
2023-04-02 12:53:47 +0800

    Remove unused code

--- src/cudaWatermark.cpp
+++ src/cudaWatermark.cpp
@@ -61,159 +61,32 @@ int main(int argc, char *argv[])
 
     findCudaDevice(argc, (const char **)argv);
 
-    std::string filename = parseArgs(argc, (const char **)argv, "input", "sloth.png");
-    int channels = -1;
-    ColoredImageType img = loadImage(filename, channels);
-    std::cout << "The image has " << channels << " channels." << std::endl;
-    
-    auto imgStack = imageChannelSplit(img, channels);
+    std::vector<std::string> imgNames{"color.png", "sloth.png"};
 
-    saveSlice(imgStack, filename, "r", 0);
-    saveSlice(imgStack, filename, "g", 1);
-    saveSlice(imgStack, filename, "b", 2);
-
-    exit(EXIT_SUCCESS);
-
-    if (checkCmdLineFlag(argc, (const char **)argv, "input"))
+    for (auto imgName : imgNames)
     {
-      getCmdLineArgumentString(argc, (const char **)argv, "input", &filePath);
+      // Step 1: get the filename and load into image.
+      std::string filename = parseArgs(argc, (const char **)argv, "input", imgName);
+      int channels = -1;
+      ColoredImageType img = loadImage(filename, channels);
+      std::cout << "The image has " << channels << " channels." << std::endl;
+
+      auto imgStack = imageChannelSplit(img, channels);
+
+      saveSlice(imgStack, filename, "r", 0);
+      saveSlice(imgStack, filename, "g", 1);
+      saveSlice(imgStack, filename, "b", 2);
+
+      // Free resources
+      FreeImage_Unload(img);
+      for (auto img : imgStack)
+        FreeImage_Unload(img);
     }
-    else
-    {
-      filePath = sdkFindFilePath("Lena.pgm", argv[0]);
-    }
-
-    if (filePath)
-    {
-      sFilename = filePath;
-    }
-    else
-    {
-      sFilename = "Lena.pgm";
-    }
-
-    // if we specify the filename at the command line, then we only test
-    // sFilename[0].
-    int file_errors = 0;
-    std::ifstream infile(sFilename.data(), std::ifstream::in);
-
-    if (infile.good())
-    {
-      std::cout << "convFilterNPP opened: <" << sFilename.data()
-                << "> successfully!" << std::endl;
-      file_errors = 0;
-      infile.close();
-    }
-    else
-    {
-      std::cout << "convFilterNPP unable to open: <" << sFilename.data() << ">"
-                << std::endl;
-      file_errors++;
-      infile.close();
-    }
-
-    if (file_errors > 0)
-    {
-      exit(EXIT_FAILURE);
-    }
-
-    // Loop over the value of conv kernel, generate different results.
-    for (int convValue = 1; convValue < 15; convValue++)
-    {
-      std::string sResultFilename = sFilename;
-
-      std::string::size_type dot = sResultFilename.rfind('.');
-
-      if (dot != std::string::npos)
-      {
-        sResultFilename = sResultFilename.substr(0, dot);
-      }
-
-      sResultFilename += "_convFilter_" + std::to_string(convValue) + ",1.pgm";
-
-      if (checkCmdLineFlag(argc, (const char **)argv, "output"))
-      {
-        char *outputFilePath;
-        getCmdLineArgumentString(argc, (const char **)argv, "output",
-                                &outputFilePath);
-        sResultFilename = outputFilePath;
-      }
-
-      // declare a host image object for an 8-bit grayscale image
-      npp::ImageCPU_8u_C1 oHostSrc;
-      // load gray-scale image from disk
-      npp::loadImage(sFilename, oHostSrc);
-      // declare a device image and copy construct from the host image,
-      // i.e. upload host to device
-      npp::ImageNPP_8u_C1 oDeviceSrc(oHostSrc);
-
-      // Start the timer for recording processing times
-      auto timerStart = std::chrono::high_resolution_clock::now();
-
-      // create struct with box-filter mask size
-      NppiSize oMaskSize = {5, 5};
-
-      NppiSize oSrcSize = {(int)oDeviceSrc.width(), (int)oDeviceSrc.height()};
-      NppiPoint oSrcOffset = {0, 0};
-
-      // create struct with ROI size
-      NppiSize oSizeROI = {(int)oDeviceSrc.width(), (int)oDeviceSrc.height()};
-      // allocate device image of appropriately reduced size
-      npp::ImageNPP_8u_C1 oDeviceDst(oSizeROI.width, oSizeROI.height);
-      // set anchor point inside the mask to (oMaskSize.width / 2,
-      // oMaskSize.height / 2) It should round down when odd
-      NppiPoint oAnchor = {oMaskSize.width / 2, oMaskSize.height / 2};
-     
-      // Try to change these values to see the difference
-      npp::ImageCPU_32s_C1 hostKernel(5,5);
-      for(int x = 0 ; x < 5; x++){
-          for(int y = 0 ; y < 5; y++){
-              hostKernel.pixels(x,y)[0].x = convValue;
-          }
-      }
-      // Set the middle pixel to be least weight.
-      hostKernel.pixels(3,3)[0].x = 1;
-      // Create device side kernel
-      npp::ImageNPP_32s_C1 pKernel(hostKernel);
-
-      // We now use Convolution fileter!
-      NPP_CHECK_NPP(nppiFilter_8u_C1R(
-          oDeviceSrc.data(), oDeviceSrc.pitch(),
-          oDeviceDst.data(), oDeviceDst.pitch(), oSizeROI,
-          pKernel.data(),
-          oMaskSize, oAnchor, 25));
-
-      // declare a host image for the result
-      npp::ImageCPU_8u_C1 oHostDst(oDeviceDst.size());
-      // and copy the device result data into it
-      oDeviceDst.copyTo(oHostDst.data(), oHostDst.pitch());
-
-      auto timerStop = std::chrono::high_resolution_clock::now();
-      auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(timerStop - timerStart);
-      std::cout << "Iteration " << convValue << " takes " << duration.count() << " ms " ;
-
-      saveImage(sResultFilename, oHostDst);
-      std::cout << "Saved image: " << sResultFilename << std::endl;
-
-      // If free them now, it will only function once.
-      // nppiFree(oDeviceSrc.data());
-      // nppiFree(oDeviceDst.data());
-    }
-
-    exit(EXIT_SUCCESS);
-  }
-  catch (npp::Exception &rException)
-  {
-    std::cerr << "Program error! The following exception occurred: \n";
-    std::cerr << rException << std::endl;
-    std::cerr << "Aborting." << std::endl;
-
-    exit(EXIT_FAILURE);
   }
   catch (...)
   {
-    std::cerr << "Program error! An unknow type of exception occurred. \n";
-    std::cerr << "Aborting." << std::endl;
+    std::cerr << "ERR: Program error! An unknow type of exception occurred. \n";
+    std::cerr << "ERR: Aborting." << std::endl;
 
     exit(EXIT_FAILURE);
     return -1;
@@ -232,12 +105,6 @@ int main(int argc, char *argv[])
   // 6.1 save iffted changed ffted channel to image file.
   // 7. Combine 3 channels into one, and save into RGB file.
   // 8. Compare initial image and watermarked image, pixel by pixel, show the max diff, avg diff. Estimate the result should be only a few digit off.
-
-  // Functions needed:
-  // Mat load_image(path)
-  // void save_image(path, Mat), void save_image(path, Mat, 'r''g''b')
-  // Mat array2Mat(cudaArray)
-  // cudaArray mat2Array(Mat)
 
   return 0;
 }
